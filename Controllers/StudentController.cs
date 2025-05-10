97f632c6b587882ec162eef6aa53b80effe0d14d
waseemkhan309 <waseem3khan333@gmail.com>
2025-05-10 23:55:22 +0500

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Student_CRUD_Operation.Data;
+using Student_CRUD_Operation.Models;
+
+namespace Student_CRUD_Operation.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+        public StudentController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
+        {
+            var student = new Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribed = viewModel.Subscribed
+            };
+            await dbContext.Students.AddAsync(student);
+            await dbContext.SaveChangesAsync();
+
+            return View();
+        }
+    }
+}
