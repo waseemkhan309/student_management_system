72ab93a8dd5e7461371ab588ddc39c9e5de6bcd1
waseemkhan309 <waseem3khan333@gmail.com>
2025-05-13 03:52:45 +0500

    Edit and delete and list of students

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Student_CRUD_Operation.Data;
 using Student_CRUD_Operation.Models;
 
@@ -33,5 +34,50 @@ namespace Student_CRUD_Operation.Controllers
 
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var student = await dbContext.Students.ToListAsync();
+            return View(student);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var student  = await dbContext.Students.FindAsync(id);
+            return View(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student viewModel)
+        {
+            var student = await dbContext.Students.FindAsync(viewModel.Id);
+            if(student is not null)
+            {
+                student.Name = viewModel.Name;
+                student.Email = viewModel.Email;
+                student.Phone = viewModel.Phone;
+                student.Subscribed = viewModel.Subscribed;
+
+            await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("list","student");
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Delete(Student viewModel)
+        {
+            var student = await dbContext.Students.FindAsync(viewModel.Id);
+
+            if (viewModel is not null) {
+                dbContext.Students.Remove(student);
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("list", "student");
+        }
+        
     }
 }
